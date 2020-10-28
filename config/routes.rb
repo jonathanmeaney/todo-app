a8458c5837bdd8f9d8e16c9314cfb5ef63e1c201
Jonathan Meaney <jon.meaney@gmail.com>
2020-10-28 16:15:18 +0000

    Adding Site controller and improving style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "site#index"
+  get '/about', to: 'site#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
