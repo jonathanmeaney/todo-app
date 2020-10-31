438f3c02f0672aa606b091c7d7f0e967d6f6fc1b
Jonathan Meaney <jon.meaney@gmail.com>
2020-10-31 12:38:32 +0000

    Adding ToDo functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201028134028) do
+ActiveRecord::Schema.define(version: 20201030184852) do
+
+  create_table "to_dos", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.boolean  "completed"
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "to_dos", ["user_id"], name: "index_to_dos_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           limit: 255
@@ -20,4 +31,5 @@ ActiveRecord::Schema.define(version: 20201028134028) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "to_dos", "users"
 end
